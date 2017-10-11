e24236335ae13a79d3c98bd958983ada44903db6
Matt Ridgway <matt.ridgway@caternet.co.uk>
2017-10-11 17:01:47 +0100

    Added sample code

---
+++ test/MySolution.RegressionTests/GoogleSearchTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace MySolution.RegressionTests
+{
+    public class GoogleSearchTests
+    {
+        [Fact(DisplayName = "GIVEN the google homepage is open WHEN searching for new york times THEN results should appear")]
+        public void NewYorkTimesSearch()
+        {
+            using (var driver = new ChromeDriver(@".\"))
+            {
+                driver.Navigate().GoToUrl("https://www.google.co.uk/");
+                driver.FindElementById("lst-ib").SendKeys("new york times");
+                driver.FindElementById("lst-ib").Submit();
+
+                var nytLink = driver.FindElementByLinkText("The New York Times - Breaking News, World News & Multimedia");
+                nytLink.Should().NotBeNull();
+
+                driver.Quit();
+            }
+        }
+    }
+}
